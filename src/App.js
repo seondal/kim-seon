5d4a39424bb287e74985b5736448eea0d7062d3b
whkakrkr <whkakrkr@gmail.com>
2021-08-03 02:13:33 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const Card = styled.a`
   color: var(--font);
 
   .profile {
-    width: 80%;
+    width: 100%;
     border-radius: 50%;
     box-shadow: white 0 2px 20px;
   }
@@ -51,7 +51,7 @@ const Card = styled.a`
   img {
     box-shadow: rgba(23, 25, 29, 0.3) 0 2px 20px;
     width: 80%;
-    border-radius: 50%;
+    border-radius: 20%;
     margin-bottom: 3px;
 
     :hover {
@@ -67,26 +67,29 @@ const App = () => {
         <img className="profile" src={profile}></img>
         <h3>Seondal</h3>
       </Card>
-
       <div className="smaller">
         <Card href="https://velog.io/@seondal">
           <img src={velog}></img>
+          Velog
         </Card>
-
         <Card href="https://whkakrkr.tistory.com">
           <img src={tistory}></img>
+          Tistory
         </Card>
       </div>
 
       <div className="smaller">
         <Card href="https://github.com/seondal">
           <img src={github}></img>
+          GitHub
         </Card>
         <Card href="https://suave-lilac-075.notion.site/fd0c2a204d8e4fd7b193800c20d5eda0?v=fc4e5ae6e85a44b1988817215a63a854">
           <img src={notion}></img>
+          Projects
         </Card>
         <Card href="https://www.instagram.com/dev_seondal/">
           <img src={insta}></img>
+          Instagram
         </Card>
       </div>
 
