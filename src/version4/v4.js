3488fa4a90e6e3bb33425f704bab875e38198a83
seondal <sseondal@gmail.com>
2022-02-19 15:13:04 +0900

    :sparkles: [Feat] map 이용하여 카드 리스트 생성

--- src/version4/v4.js
+++ src/version4/v4.js
@@ -1,7 +1,32 @@
 import styled from "styled-components";
+import Card from "./Card";
+import velogIcon from "../version3/assets_v3/velog.png";
+
+const CardContents = [
+  {
+    link: "https://velog.io/@seondal",
+    icon: velogIcon,
+    name: "seondal.log",
+    detail: "개발블로그",
+    color: "#1ec592",
+  },
+  {
+    link: "https://velog.io/@seondal",
+    icon: velogIcon,
+    name: "seondal.log",
+    detail: "개발블로그",
+    color: "#1ec592",
+  },
+];
 
 const v4 = () => {
-  return <>hi</>;
+  return (
+    <>
+      {CardContents.map((content) => (
+        <Card contents={content} />
+      ))}
+    </>
+  );
 };
 
 export default v4;
