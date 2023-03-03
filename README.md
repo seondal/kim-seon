b43f1bf0889c8688df71f8002f80995811b71269
Kim Seon <sseondal@gmail.com>
2023-03-03 20:14:49 +0900

    change github link

--- README.md
+++
@@ -1,1 +0,0 @@
-[ReadMe](https://www.notion.so/Kimseon-a27d44c07ee145cc94830c569e642e80)
--- src/version3/v3.js
+++ src/version3/v3.js
@@ -127,10 +127,9 @@ const v3 = () => {
         <Card
           className="github"
           style={{ width: "130px", gap: "5%" }}
-          href="https://suave-lilac-075.notion.site/fd0c2a204d8e4fd7b193800c20d5eda0?v=c62e2af146ed446a97b34c86c16d4835"
-        >
+          href="https://github.com/seondal">
           <img className="icon" src={github}></img>
-          <div className="name"> Projects</div>
+          <div className="name"> Github </div>
         </Card>
 
         <Card
