61334ec2f00206506995660cd1fffa7a1ef86035
seondal <sseondal@gmail.com>
2023-12-05 11:25:12 +0900

    ✂️ edit :  벨로그 링크 변경

--- src/components/Velog.tsx
+++ src/components/Velog.tsx
@@ -1,9 +1,11 @@
+const LINK = "name=seondal&tag=인생네컷";
+
 export default function Velog() {
   return (
-    <a href="https://velog-readme-stats.vercel.app/api/redirect?name=seondal">
+    <a href={`https://velog-readme-stats.vercel.app/api/redirect?${LINK}`}>
       <img
         className="image"
-        src="https://velog-readme-stats.vercel.app/api?name=seondal&tag=인생네컷"
+        src={`https://velog-readme-stats.vercel.app/api?${LINK}`}
         alt=""
       />
     </a>
