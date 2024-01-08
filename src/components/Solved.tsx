e2db1e6bc5eb66a569a2c5b37bf0b481b4f56f38
seondal <sseondal@gmail.com>
2024-01-08 20:08:33 +0900

    💄 style :  솔브닥 디자인 추가

--- src/components/Solved.tsx
+++ src/components/Solved.tsx
@@ -1,11 +1,13 @@
+const Mazza = "http://mazassumnida.wtf/api/v2/generate_badge?boj=whkakrkr";
+const White =
+  "https://github-readme-solvedac.hyp3rflow.vercel.app/api/?handle=whkakrkr";
+const Zandi = "http://mazandi.herokuapp.com/api?handle=whkakrkr&theme=warm";
+
 export default function Solved() {
   return (
     <a href="https://solved.ac/whkakrkr">
-      <img
-        className="image"
-        src="http://mazassumnida.wtf/api/v2/generate_badge?boj=whkakrkr"
-        alt=""
-      />
+      <img className="image" src={White} alt="" />
+      <img className="image" src={Zandi} alt="" />
     </a>
   );
 }
